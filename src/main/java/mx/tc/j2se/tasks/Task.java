d0e1c42743ebf3834c3c4bf99ea2f4e7ff32eada
fjoaq <fjaoquintrujillo@gmail.com>
2022-06-22 13:39:35 -0500

    Commit: Practice 1, in this version the Javadocs were added.

--- src/main/java/mx/tc/j2se/tasks/Task.java
+++ src/main/java/mx/tc/j2se/tasks/Task.java
@@ -1,28 +1,74 @@
 package mx.tc.j2se.tasks;
 
 public interface Task {
+    /**
+     * This method is for reading the task name
+     * @return  the task name.
+     */
     String getTitle();
-
+    /**
+     * This method is for setting the task name
+     * @param title the task name.
+     */
     void setTitle(String title);
-
+    /**
+     * This method is for reading the task status
+     * @return true if the task is active.
+     */
     boolean isActive();
-
+    /**
+     * This method is for setting the task status
+     * @param active  true for active and false for inactive tasks.
+     */
     void setActive(boolean active);
-
+    /**
+     * This method is for get the time of execution of a non-repetitive task.
+     * If the task is a repetitive one, return the start time of the repetition
+     * @return the time of execution.
+     */
     int getTime();
-
+    /**
+     * This method is for set time of a non-repetitive task.
+     * If the task was a repetitive it should become non-repetitive
+     *  @param time the time of execution.
+     */
     void setTime(int time);
-
+    /**
+     * This method is for get the start time of a repetitive task.
+     * If the task is a non-repetitive one, return the time of execution
+     * @return the star time.
+     */
     int getStartTime();
-
+    /**
+     * This method is for get the end time of a repetitive task.
+     * If the task is a non-repetitive one, return the time of execution
+     * @return the end time.
+     */
     int getEndTime();
-
+    /**
+     * This method is for get the interval time of a repetitive task.
+     * If the task is a non-repetitive one, return 0
+     * @return the repetitive interval
+     */
     int getRepeatInterval();
-
+    /**
+     * This method is for set time of a repetitive task.
+     * If the task is a non-repetitive one, it should become repetitive
+     *  @param start the start time.
+     *  @param end the end time.
+     *  @param interval the interval time.
+     */
     void setTime(int start, int end, int interval);
-
+    /**
+     * This method is for checking the task repeatability
+     * @return the boolean variable "repeated".
+     */
     boolean isRepeated();
-
+    /**
+     * This method is for checking the next task execution.
+     * @param current the current time.
+     * @return the next task.
+     */
     int nextTimeAfter(int current);
 
 }
--- src/main/java/mx/tc/j2se/tasks/TaskImpl.java
+++ src/main/java/mx/tc/j2se/tasks/TaskImpl.java
@@ -29,29 +29,27 @@ public class TaskImpl implements Task{
         repeated = true;
     }
 
-    //For reading the task name
+
     @Override
     public String getTitle(){
         return title;
     }
-    //For setting the task name
+
     @Override
     public void setTitle(String title){
         this.title = title;
     }
-    //For reading the task status
+
     @Override
     public boolean isActive(){
         return active;
     }
-    //For setting the task status
+
     @Override
     public void setActive(boolean active){
         this.active = active;
     }
 
-    // Methods for non-repetitive tasks
-    //If the task is a repetitive one, return the start time of the repetition
     @Override
     public int getTime(){
         if(repeated){
@@ -60,7 +58,7 @@ public class TaskImpl implements Task{
             return time;
         }
     }
-    //If the task was a repetitive it should become non-repetitive
+
     @Override
     public void setTime(int time){
         if(repeated){
@@ -70,8 +68,7 @@ public class TaskImpl implements Task{
             this.time = time;
         }
     }
-    //Methods for repetitive tasks
-    //If the task is a non-repetitive one, return the time of execution
+
     @Override
     public int getStartTime(){
         if (!repeated){
@@ -80,7 +77,7 @@ public class TaskImpl implements Task{
             return start;
         }
     }
-    //If the task is a non-repetitive one, return the time of execution
+
     @Override
     public int getEndTime(){
         if (!repeated){
@@ -89,7 +86,7 @@ public class TaskImpl implements Task{
             return end;
         }
     }
-    //If the task is a non-repetitive one, return 0
+
     @Override
     public int getRepeatInterval(){
         if (!repeated){
@@ -98,7 +95,7 @@ public class TaskImpl implements Task{
             return interval;
         }
     }
-    //If the task is a non-repetitive one, it should become repetitive
+
     @Override
     public void setTime(int start, int end, int interval){
         if (!repeated){
@@ -113,12 +110,12 @@ public class TaskImpl implements Task{
         }
     }
 
-    //To check the repeatability
+
     public boolean isRepeated(){
         return repeated;
     }
 
-    //To check the next task execution
+
     public int nextTimeAfter(int current){
         int i=0;
         if(!active){
